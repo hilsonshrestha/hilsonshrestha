c675a21b9b3f017f4b671a109370da159ecd9928
Hilson Shrestha <hilsonshrestha@gmail.com>
2014-09-22 22:29:27 +0545

    Initialized portfolio

---
+++ controllers/portfolio.py
@@ -0,0 +1,6 @@
+from base_handler import *
+import logging
+
+class PortfolioPage(BaseHandler):
+	def get(self):
+		self.write("I am working on it :)")
--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import webapp2
 
 from controllers.home import HomePage
 from controllers.blog import BlogPage, BlogArticlePage, BlogAdminPage
+from controllers.portfolio import PortfolioPage
 app = webapp2.WSGIApplication([
 		('/', HomePage),
+
 		('/blog', BlogPage),
 		('/blog/admin', BlogAdminPage),
 		('/blog/([^/]+)', BlogArticlePage),
+
+		('/portfolio', PortfolioPage),
 	], debug = True)
